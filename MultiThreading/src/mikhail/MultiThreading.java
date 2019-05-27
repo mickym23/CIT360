43555b6be4a4c3dabf739488f4ddd75fba35cd02
User <User@User-PC>
2019-05-27 23:31:22 +0200

    New Threads Commit

--- MultiThreading/src/mikhail/MultiThreading.java
+++ MultiThreading/src/mikhail/MultiThreading.java
@@ -1,68 +1,70 @@
 package mikhail;
-import java.text.SimpleDateFormat;  
-import java.util.Date; 
-import java.util.concurrent.*;
 
-public class MultiThreading implements Runnable {
-			
-			public static void main(String[] args) {
-
-		    // Five Threads are created:
-	        Runnable th1 = new MultiThreading("Thread 1"); 
-	        Runnable th2 = new MultiThreading("Thread 2"); 
-	        Runnable th3 = new MultiThreading("Thread 3"); 
-	        Runnable th4 = new MultiThreading("Thread 4"); 
-	        Runnable th5 = new MultiThreading("Thread 5");       
-	        
-	        // Maximum size of Threadpool: 5
-	        ExecutorService tp = Executors.newFixedThreadPool(5);   
-	         
-	        // Executes threads 
-	        tp.execute(th1); 
-	        tp.execute(th2); 
-	        tp.execute(th3); 
-	        tp.execute(th4); 
-	        tp.execute(th5);  
-	          
-	        // Executor Service stops all Client tasks incoming 
-	        tp.shutdown();
-			} 
-
-			// Declaring thread variable
-		 	public String thread; 
-	     
-		 	// Constructor
-		    public MultiThreading(String task) { 
-		        thread = task; 
-		    } 
-		       
-		    public void run() { 
-		       try { 
-		          // For loop iterates through each thread
-		          for (int i = 0; i<=5; i++) { 
-		              if (i==0) {
-		            	  
-		                 // Adding Date and Time for speed analysis
-		                 Date d = new Date(); 
-		                 SimpleDateFormat format = new SimpleDateFormat("  hh:mm:ss, yyyy/MM/dd");
-		                  
-		                 // Initialization of Thread, Date, and Time is printed
-		                 System.out.println("Initializing: " + thread + " = " + format.format(d));  
-		              } 
-		              else {
-		                 Date d = new Date(); 
-		                 SimpleDateFormat format = new SimpleDateFormat("   hh:mm:ss, yyyy/MM/dd");
-		                 
-		                 // Execution of Thread, Date, and Time is printed
-		                 System.out.println("Executing: "+  thread + " = " +format.format(d));      
-		              } 
-		              Thread.sleep(1000); 
-		         } 
-		         System.out.println("Completed: " + thread); 
-		      } 
-		      catch(InterruptedException e1) {
-		    	 // Will print a Java error backtrace to the exception 
-		         e1.printStackTrace(); 
-		      } 
-		  }     
+public class MultiThreading {
+	
+	/* This program shows incrementing threads.
+	 * It shows each thread as they are created and completed.
+	 * Several exceptions can be thrown and are accounted for.
+	 */
+	
+	// Thread output: name + value
+	 public static void threadOut(int increment) {
+		String thread = Thread.currentThread().getName();
+		System.out.println(thread + "'s Current Increment: " + increment);
+	 }
+		
+	 // Runnable interface used
+     private static class threadLoop implements Runnable {
+	 public void run() { 
+		 try {
+			 // integer array used as increment, couldn't figure out
+			 // how to add it using a FOR loop
+			int[] loop = {
+					0,
+					1, 
+					2,
+					3,
+					4,
+					5,
+					6,
+					7,
+					8,
+					9,
+					10
+		};
+		try {
+			// Passes the increment value to the output method at 1 second intervals
+			for (int i = 0; i < loop.length; i++) {
+				Thread.sleep(1000);
+				threadOut(loop[i]);
+			}
+		// Two possible exceptions dealt with	
+		} catch (InterruptedException e) {
+			System.err.println("Error: Not completed.");
+		}
+	  } catch (RuntimeException e) {
+		  System.err.println("Something went wrong!");
+	  }
+		 
+    }
+}
+     
+    // Main method organizes the order in which everything runs
+	public static void main(String[] args) {
+      System.out.println("Beginning Thread Increments");
+      
+      // Creates new threadLoop class and initiates it
+      Thread thread = new Thread(new threadLoop());
+      thread.start();
+      while (thread.isAlive()) {
+         System.out.println("\tInitiating!");
+         try {
+        	// Join method waits until the previous thread is completed 
+			thread.join(700);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+      }
+     System.out.println((" Threads Successfully Completed."));
 	}
+} 
