abf8cbf3fd112835419c3a7e5e6457c6ecf643c0
User <User@User-PC>
2019-06-03 16:36:00 +0200

    ACP commit with comments

--- JavaProj_AppController/src/acp/ACP.java
+++ JavaProj_AppController/src/acp/ACP.java
@@ -3,14 +3,29 @@ package acp;
 import java.util.HashMap;
 import java.util.Scanner;
 
+/* This program is designed to display the uniqueness of using a 
+ * HashMap in the Application Controller Pattern. There is a separate handler 
+ * program that uses the Helper classes connect together to display the result. 
+ */
+
+/* This is the ACP class, here the main method is found. It is the 
+ * main interface which the user interacts with. Here, it calls methods from 
+ * the App class which outputs to the display. 
+ */
 public class ACP {
 
+	// out variable created to connect to App class
 	@SuppressWarnings("unused")
 	private static App out;
 
 	public static void main(String[] args) {
+		// input
 		Scanner input = new Scanner(System.in);
+		
+		// connect to the App class
 		out = new App();
+		
+		// Filler variables to initialize the method.
 		String brand = "Test";
 		int price = 0;
 		int speed = 0;
@@ -18,34 +33,48 @@ public class ACP {
 		
 		System.out.println("\t\t\tSupercar Showroom");
 		
+		// Ask the user for car brand input
 		System.out.print("Enter a car brand(Ferrari, Bugatti, Aston Martin, or Mercedes): ");
 		req = input.nextLine();
 		
+		// Outputs the user's request
 		System.out.println("\n\t\t\tCar requested: " + req);
+		
+		// Call the handleCommand method from the App class
 		App.handleCommand(req, brand, price, speed);
 		
 		input.close();
 	}
 }
 
+/* This class acts as the Controller section of the program.
+ * Here, the HashMap is created and connected to the various Helpers classes
+ * It also calls the Handler interface to execute the classes. 
+ */
 class App {
+	
+	// public HashMap is created
 	public static HashMap<String, HandleReq> m = new HashMap<String, HandleReq>();
 	
+	// No-args constructor
 	public App () {
 		
 	}
 	
+	// method that connects the user input to the required Helper class' methods
 	public static void handleCommand(String req, String brand, int price, int speed) {
 		m.put("Ferrari", new Ferrari());
 		m.put("Bugatti", new Bugatti());
 		m.put("Aston Martin", new AM());
 		m.put("Mercedes", new Mercedes());
 		
+		// execute the needed class
 		HandleReq handle = m.get(req);
 		handle.execute(brand, price, speed);
 	}
 }
 
+/* Helper class that outputs Ferrari's specifications */
 class Ferrari implements HandleReq {
 	public void execute(String brand, int price, int speed) {
 		brand = "2018 Ferrari 812 Superfast";
@@ -57,6 +86,7 @@ class Ferrari implements HandleReq {
 	}
 }
 
+/* Helper class that outputs Bugatti's specifications */
 class Bugatti implements HandleReq {
 	public void execute(String brand, int price, int speed) {
 		brand = "2019 Bugatti Chiron";
@@ -68,6 +98,7 @@ class Bugatti implements HandleReq {
 	}
 }
 
+/* Helper class that outputs Aston Martin's specifications */
 class AM implements HandleReq {
 	public void execute(String brand, int price, int speed) {
 		brand = "2015 Aston Martin Vulcan";
@@ -79,6 +110,8 @@ class AM implements HandleReq {
 	}
 }
 
+
+/* Helper class that outputs Mercedes' specifications */
 class Mercedes implements HandleReq {
 	public void execute(String brand, int price, int speed) {
 		brand = "2017 Mercedes-AMG Project One";
--- JavaProj_AppController/src/acp/HandleReq.java
+++ JavaProj_AppController/src/acp/HandleReq.java
@@ -1,5 +1,9 @@
 package acp;
 
+/* Handler-type interface. 
+ * This allows for data to go from the Helper class to the 
+ * App class and eventually out to the user.
+ */
 public interface HandleReq {
 	void execute(String brand, int price, int speed);
 }
